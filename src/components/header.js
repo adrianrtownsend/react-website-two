7a6ffd745479f75d3d112854d5488cfd45419c0c
Adrian Townsend <greenlight8004@gmail.com>
2021-10-13 15:36:15 -0400

    fixing header toggle

--- src/components/header.js
+++ src/components/header.js
@@ -1,67 +1,41 @@
+import { useState } from  'react'
+
 import '../assets/scss/style.scss';
 
 import logo from '../assets/images/logo.svg';
 
-/*
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-// const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
-const altFadeElems = document.querySelectorAll('.alt-has-fade');
-
- function btnHamburgerClick(e) {
-  e.preventDefault();
-  console.log('click hamburger');
-
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
-    });
-    altFadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-    
-  }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach(function(element){
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-    altFadeElems.forEach(function(element){
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
-    });
-
-  } 
-}
-*/
 
 export default function Header() {
-  return (
-    <header className="header">
-      <div className="overlay has-fade"/>
-      <div className="overlay_block has-fade" />
-      <nav>
-        {/*<a id="btnHamburger" onClick={btnHamburgerClick} href="/" className="header__toggle hide-for-desktop">
-          <span/>
-          <span/>
-          <span/>
-        </a>*/}
 
-        <a href="/" className="header__logo alt-has-fade">
+  const [headerOpen, setHeaderOpen] = useState(true)
+
+  const toggleHeader = () => {
+    setHeaderOpen(prevState => {
+      const body = document.querySelector('body');
+      prevState ? body.classList.remove('noScroll') : body.classList.add('noScroll')
+      return !prevState
+    })
+  }
+
+  return (
+    <header className={`header ${headerOpen ? '' : 'open'}`}>
+      <div className={`overlay ${headerOpen ? 'fade-out' : 'fade-in'}`}/>
+      <div className={`overlay_block ${headerOpen ? 'fade-out' : 'fade-in'}`} />
+      <nav>
+        <span id="btnHamburger" onClick={() => toggleHeader()} href="/" className="header__toggle hide-for-desktop">
+          <span/>
+          <span/>
+          <span/>
+        </span>
+
+        <a href="/" className={`header__logo ${headerOpen ? 'fade-in' : 'fade-out'}`}>
           <img src={logo} alt="logo" />
         </a>
 
         <div className="hide-for-desktop" />
        
 
-        <div className="header__menu has-fade hide-for-desktop">
+        <div className={`header__menu header__menu__mobile hide-for-desktop ${headerOpen ? 'fade-out' : 'fade-in'}`}>
           <a href="/">home</a>
           <a href="/">shop</a>
           <a href="/">about</a>
